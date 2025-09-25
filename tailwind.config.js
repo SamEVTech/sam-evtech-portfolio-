6dec69b40372aa74db80e90a9429ecb038972333
samir mulla <samirmulla2007@gmail.com>
2025-09-25 16:53:09 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'blue-900': '#1E3A8A',
+        'emerald-500': '#10B981',
+      },
+    },
+  },
+  plugins: [],
+};
